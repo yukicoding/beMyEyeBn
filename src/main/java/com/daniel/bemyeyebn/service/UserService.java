f9a6b94e08af1fa03567d8a920c84202b990f0cf
yukicoding <hyq1046410512@gmial.com>
2022-04-25 22:52:16 +0800

    feat： hutool

--- src/main/java/com/daniel/bemyeyebn/service/UserService.java
+++ src/main/java/com/daniel/bemyeyebn/service/UserService.java
@@ -1,5 +1,9 @@
 package com.daniel.bemyeyebn.service;
 
+import cn.hutool.core.lang.Console;
+import cn.hutool.core.util.CharsetUtil;
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpUtil;
 import com.daniel.bemyeyebn.mapper.UserMapper;
 import com.daniel.bemyeyebn.model.User;
 
@@ -9,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -18,20 +23,13 @@ public class UserService {
     @Autowired
     UserMapper userMapper;
     public List<User> getAllUser() throws Exception {
-        GetExample getExample = new GetExample();
-        PostExample postExample = new PostExample();
-//        String response = getExample.run("https://raw.github.com/square/okhttp/master/README.md");
-//        System.out.println(response);
-        String response2 = postExample.post("http://www.roundsapp.com/post","{'winCondition':'HIGH_SCORE',"
-                + "'name':'Bowling',"
-                + "'round':4,"
-                + "'lastSaved':1367702411696,"
-                + "'dateStarted':1367702378785,"
-                + "'players':["
-                + "{'name':'" + "qiqi" + "','history':[10,8,6,7,8],'color':-13388315,'total':39},"
-                + "{'name':'" + "hyq" + "','history':[6,10,5,10,10],'color':-48060,'total':41}"
-                + "]}");
-        System.out.println(response2);
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("key","33da32fe2ab241e0ab87f0099349e15c");
+
+        String result = HttpRequest.get("https://geoapi.qweather.com/v2/city/top?key=33da32fe2ab241e0ab87f0099349e15c")
+                .header("Content-Type","application/x-www-form-urlencoded;charset=UTF-8").execute().body();
+    System.out.println(result);
+
         return userMapper.getAllUser();
     }
     public Integer addUser(User user){
